48e7a764a50e9112cac724bfbbc21f2ef581ffe4
Grigory Petrov <grigory.v.p@gmail.com>
2013-06-24 11:44:16 +0400

    Refactoring.

--- pyico/__init__.py
+++ pyico/__init__.py
@@ -19,7 +19,7 @@ class Ico( object ):
 
   def __init__( self ):
     self.images_l = []
-    self.writer_o = Writer()
+    self.writer_o = binary.Writer()
 
 
   ##  Evaluates to binary data corresponding to this icon. It can be used
@@ -95,118 +95,6 @@ class Image( object ):
 
   def __str__( self ):
     return "{width_n}x{height_n}x{bpp_n}".format( ** self.__dict__ )
-
-
-class Writer( object ):
-
-
-  def __init__( self ):
-    self.chunks_l = []
-
-
-  def data( self ):
-    sData = ""
-
-    ##  Data that has 'end' flag must be written after data without it.
-    self.chunks_l = [ o for o in self.chunks_l if not o[ 'end_f' ] ] + \
-      [ o for o in self.chunks_l if o[ 'end_f' ] ]
-
-    ##  Calculate sizes and offsets of all data, so items marked as
-    ##  'size' and 'offset' can be assigned correct values.
-    nOffset = 0
-    for mChunk in self.chunks_l:
-      if mChunk[ 'type_s' ] in [ 'offset', 'size' ]:
-        mChunk[ 'size_n' ] = len( struct.pack( mChunk[ 'format_s' ], 0 ) )
-      else:
-        mChunk[ 'size_n' ] = len( mChunk[ 'data_s' ] )
-      mChunk[ 'offset_n' ] = nOffset
-      nOffset += mChunk[ 'size_n' ]
-
-
-    for mChunk in self.chunks_l:
-      if mChunk[ 'type_s' ] in [ 'offset', 'size' ]:
-        ##  Find chunk whose offset or size must be written.
-        nId = mChunk[ 'target_n' ]
-        lChunks = [ o for o in self.chunks_l if o[ 'id_n' ] == nId ]
-        ##  Id not found or not unique?
-        assert 1 == len( lChunks )
-        if 'offset' == mChunk[ 'type_s' ]:
-          nVal = lChunks[ 0 ][ 'offset_n' ]
-        if 'size' == mChunk[ 'type_s' ]:
-          nVal = lChunks[ 0 ][ 'size_n' ]
-        sData += struct.pack( mChunk[ 'format_s' ], nVal )
-      else:
-        sData += mChunk[ 'data_s' ]
-
-    return sData
-
-
-  def clear( self ):
-    self.chunks_l = []
-
-
-  def write( self, s_format, * args ):
-    self._write(
-      s_format = s_format,
-      f_end = False,
-      n_id = None,
-      args = args )
-
-
-  def writeEnd( self, s_format, * args ):
-    self._write(
-      s_format = s_format,
-      f_end = True,
-      n_id = None,
-      args = args )
-
-
-  def writeArrayEnd( self, s_data, n_id = None ):
-    self._write(
-      s_format = None,
-      f_end = True,
-      n_id = n_id,
-      args = [ s_data ] )
-
-
-  def writeOffset( self, s_format, n_offsetId ):
-    self.chunks_l.append({
-      'type_s': 'offset',
-      'format_s': s_format,
-      'target_n': n_offsetId,
-      'end_f': False,
-      'id_n': None,
-    })
-
-
-  def writeSize( self, s_format, n_sizeId ):
-    self.chunks_l.append({
-      'type_s': 'size',
-      'format_s': s_format,
-      'target_n': n_sizeId,
-      'end_f': False,
-      'id_n': None,
-    })
-
-
-  def _write( self, s_format, f_end, n_id, args ):
-    ##  Write some integers in specified binary format?
-    if s_format:
-      self.chunks_l.append({
-        'type_s': 'data',
-        'data_s': struct.pack( s_format, * args ),
-        'end_f': f_end,
-        'id_n': n_id,
-      })
-    ##  Write array?
-    else:
-      assert 1 == len( args )
-      self.chunks_l.append({
-        'type_s': 'array',
-        'data_s': args[ 0 ],
-        'end_f': f_end,
-        'id_n': n_id,
-      })
 
 
 class ReaderIco( binary.Reader ):
